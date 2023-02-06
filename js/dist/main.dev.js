0f817cc4d942390709343e1e06d74abfc6571593
Abdallah mohamady bakry Elhosary <114493396+AbdallahElhosary@users.noreply.github.com>
2023-02-06 23:37:43 +0200

    Add files via upload

---
+++ js/dist/main.dev.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Start swiper Photos
+var swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  // slidesPerView: 1,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 20
+    }
+  }
+});
